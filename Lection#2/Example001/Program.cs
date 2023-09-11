aaf0a955bc8a94a19ed8687c494065f85bcff74d
Nikita <Wekly1@gmail.com>
2023-09-11 17:55:56 +0300

    Example001 first function

--- Lection#2/Example001/Program.cs
+++ Lection#2/Example001/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//
+
+Console.Clear();
+
+int a1 = 10;
+int b1 = 12;
+int c1 = 313;
+int a2 = 13;
+int b2 = 41;
+int c2 = 21;
+int a3 = 22;
+int b3 = 32;
+int c3 = 33;
+
+int Max(int arg1, int arg2, int arg3)
+{
+int result = arg1;
+if (arg2 > result) result = arg2;
+if (arg3 > result) result = arg3;
+return result;
+}
+
+//int max1 = Max (a1, b1, c1);
+//int max2 = Max (a2, b2, c2);
+//int max3 = Max (a3, b3, c3);
+//int max = Max (max1, max2, max3);
+int max = Max (Max (a1, b1, c1), Max (a2, b2, c2), Max (a3, b3, c3));
+
+System.Console.WriteLine($"max = {max}");
