68ae36a04f14e9c1eb7afd4c632a6d7f9e5fddde
Nikita <Wekly1@gmail.com>
2023-09-12 14:21:13 +0300

    Completed Example002

--- Lection#3/Example002/Program.cs
+++ Lection#3/Example002/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Цикл for
+// Вид 1
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(5, "A");
+System.Console.WriteLine(res);
+
+// Вид 2
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        System.Console.WriteLine($"{i}x{j}={i*j}");
+    }
+    System.Console.WriteLine();
+}
