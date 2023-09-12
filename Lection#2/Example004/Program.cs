04f615dc3941be1a88575bc7038b731b8e6abe65
Nikita <Wekly1@gmail.com>
2023-09-12 09:37:10 +0300

    Completed example004

--- Lection#2/Example004/Program.cs
+++ Lection#2/Example004/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection) // заполнение массива случайными числами
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        Console.Write(", ");
+        position++;
+    }
+}
+
+int[] array = new int[10]; // new int[10] - создай новый массив с длинной 10
+
+FillArray(array);
+PrintArray(array);
