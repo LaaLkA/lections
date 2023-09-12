a5bc429dd7473dd2d7f0b35e159c6a28100f88db
Nikita <Wekly1@gmail.com>
2023-09-12 09:41:44 +0300

    Created a new folder example005

--- Lection#2/Example005/Program.cs
+++ Lection#2/Example005/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection) // заполнение массива случайными числами
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        Console.Write(", ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // new int[10] - создай новый массив с длинной 10
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+
+int pos = IndexOf(array);
+System.Console.WriteLine(pos);
