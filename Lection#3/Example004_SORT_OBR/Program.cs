7052ac53e939a7a3d585665ea1efea4f3049046f
Nikita <Wekly1@gmail.com>
2023-09-12 16:53:00 +0300

    Completed homework Lection#3 Example004 resort

---
+++ Lection#3/Example004_SORT_OBR/Program.cs
@@ -0,0 +1,35 @@
+﻿// Алгоритм сортировки массива методом выбора
+
+Console.Clear();
+
+int[] arr = {3, 1, 6, 3, 5, 7, 9, 8, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
