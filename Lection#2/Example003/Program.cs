e1d08ca14ca6493a35dc8ff9be22b1c557e29111
Nikita <Wekly1@gmail.com>
2023-09-12 09:26:47 +0300

    Completed Example003

--- Lection#2/Example003/Program.cs
+++ Lection#2/Example003/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 1, 2, 3, 4, 5, 4, 7, 8 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
